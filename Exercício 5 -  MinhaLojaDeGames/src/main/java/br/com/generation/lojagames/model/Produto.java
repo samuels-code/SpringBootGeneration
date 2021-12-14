0b27d17d72867a656d9258f96b49230c6aca64e7
Samuel Silva <93296784+samuels-code@users.noreply.github.com>
2021-12-13 23:30:21 -0300

    Loja de Games

---
+++ Exercício 5 -  MinhaLojaDeGames/src/main/java/br/com/generation/lojagames/model/Produto.java
@@ -0,0 +1,86 @@
+package br.com.generation.lojagames.model;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table (name = "tb_produtos")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull(message = "É obrigatório escrever o nome")
+	@Size(min=5)
+	private String nome;
+	
+	@Size(max = 1000)
+	private String descricao;
+	
+	@NotNull (message = "É obrigatório escrever o preço")
+	@Positive (message = "O número não pode ser negativo") //
+	private BigDecimal preco; //troquei o double, pois olhei o exemplo do professor e pesquisei a respeito
+
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+	
+	
+	
+	
+
+}
