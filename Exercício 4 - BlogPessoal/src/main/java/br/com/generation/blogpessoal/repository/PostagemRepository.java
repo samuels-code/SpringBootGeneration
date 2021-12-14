7a68363b055c1cad94f2b54ce9060418dc0833cb
Samuel Silva <93296784+samuels-code@users.noreply.github.com>
2021-12-13 23:28:57 -0300

    Blog Pessoal

---
+++ Exercício 4 - BlogPessoal/src/main/java/br/com/generation/blogpessoal/repository/PostagemRepository.java
@@ -0,0 +1,16 @@
+package br.com.generation.blogpessoal.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.generation.blogpessoal.model.Postagem;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+	
+	public List <Postagem> findAllByTituloContainingIgnoreCase(String titulo);
+	// igual o select * from tb_postagens where titulo like "%titulo%" do MY SQL
+
+}
